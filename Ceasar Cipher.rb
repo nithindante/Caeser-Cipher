ba419c85781f99b0d8492294f88dc33b9c180bec
nithindante <Rajkumar_nithin@yahoo.com>
2023-12-31 03:30:47 +0530

    Add the ruby program to the codebase

--- Ceasar Cipher.rb
+++ Ceasar Cipher.rb
@@ -1,13 +1,22 @@
 require 'pry-byebug'
 
 def caesar_cipher(string,num)
-
-print string.to_i
-
-
+    arr = string.split('')
+    newArr = []  
+    arr.map do |letter|
+      if letter.ord <=64 || (letter.ord).between?(90, 96)
+         newArr.push(letter)
+         next
+       elsif letter.ord >= 122 - num || letter.ord.between?(90 - num,90)
+         a = letter.ord - (122 - num)
+         letter = 96 + a
+         newArr.push(letter.chr)      
+       else
+         letter = letter.ord + num
+          newArr.push(letter.chr)
+       end
+     end
+     print newArr.join
 end
 
-
 caesar_cipher("What a string!", 5)
-
-"Bmfy f xywnsl!"
