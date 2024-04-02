eb2fb38431299ef66ca239278021afffdfca16ef
nithindante <Rajkumar_nithin@yahoo.com>
2024-04-03 01:19:02 +0530

    Created 6 tests for TDD approach using RSPEC

--- lib/Ceasar_Cipher.rb
+++ lib/Ceasar_Cipher.rb
@@ -1,23 +1,34 @@
 require 'pry-byebug'
-
 class Caesar_cipher
-def caesar_cipher(string,num)
+  def caesar_cipher(string,num)
+    unless /\A[\p{L}\s\p{P}]+\z/.match?(string)
+        return "Please enter a String"
+     else
+    if num.class != Integer
+      return "Please enter a String"
+    else
     arr = string.split('')
     newArr = []
     arr.map do |letter|
-      if letter.ord <=64 || (letter.ord).between?(90, 96)
+      if letter.to_i.to_s==true
+        return "please enter a String"
+
+      else
+        if letter.ord <=64 || (letter.ord).between?(90, 96)
          newArr.push(letter)
          next
-       elsif letter.ord >= 122 - num || letter.ord.between?(90 - num,90)
-         a = letter.ord - (122 - num)
-         letter = 96 + a
-         newArr.push(letter.chr)
-       else
-         letter = letter.ord + num
+        elsif letter.ord >= 122 - num || letter.ord.between?(90 - num,90)
+          a = letter.ord - (122 - num)
+          letter = 96 + a
           newArr.push(letter.chr)
-       end
+        else
+          letter = letter.ord + num
+          newArr.push(letter.chr)
+        end
+      end
      end
+  end
      return newArr.join
+  end
 end
 end
-caesar_cipher("What a string!",5)
--- spec/Ceaser_Cipher_Spec.rb
+++ spec/Ceaser_Cipher_Spec.rb
@@ -3,9 +3,44 @@ require './lib/Ceasar_Cipher'
 
 describe Caesar_cipher do
   describe '#Caesar_cipher' do
-    it 'Caesar_cipher on the Strings' do
-      caesar_cipher = Caesar_cipher.new
-      expect(caesar_cipher.caesar_cipher("What a string!", 5)).to eql("Bmfy f xywnsl!")
+    it 'Caesar_cipher when working on normal strings' do
+      #caesar_cipher = Caesar_cipher.new
+      expect(subject.caesar_cipher("What a string!",5)).to eql("Bmfy f xywnsl!")
+    end
+  end
+
+  describe '#Caesar_cipher' do
+    it 'Caesar_cipher when no input is given' do
+      #caesar_cipher = Caesar_cipher.new
+      expect(subject.caesar_cipher("",0)).to eql("Please enter a String")
+    end
+  end
+
+  describe '#Caesar_cipher' do
+    it 'Caesar_cipher when negative shift is given' do
+      #caesar_cipher = Caesar_cipher.new
+      expect(subject.caesar_cipher("What a string!",-5)).to eql("Rc\\o \\ nomdib!")
+    end
+  end
+
+  describe '#Caesar_cipher' do
+    it 'Caesar_cipher when input is not a string is given' do
+      #caesar_cipher = Caesar_cipher.new
+      expect(subject.caesar_cipher("123",5)).to eql("Please enter a String")
+    end
+  end
+
+  describe '#Caesar_cipher' do
+    it 'Caesar_cipher when when huge negative shift is given' do
+      #caesar_cipher = Caesar_cipher.new
+      expect(subject.caesar_cipher("Hello,my name is Ruby",-34)).to eql("&CJJM,KW L?KC GQ 0S@W")
+    end
+  end
+
+  describe '#Caesar_cipher' do
+    it 'Caesar_cipher when input for shift is not a integer' do
+      #caesar_cipher = Caesar_cipher.new
+      expect(subject.caesar_cipher("Hello,my name is Ruby","a")).to eql("Please enter a String")
     end
   end
 end
